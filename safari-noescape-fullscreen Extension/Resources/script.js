820eebd93c4637bde607bedc311fb5006831250e
PauloLeal <paulolealdev@gmail.com>
2023-02-11 12:49:03 -0300

    clean app

--- safari-noescape-fullscreen Extension/Resources/script.js
+++ safari-noescape-fullscreen Extension/Resources/script.js
@@ -1,1 +1,8 @@
 console.log("[safari-noesc] loaded");
+
+window.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        console.log('[safari-noesc] Esc suppressed');
+        event.preventDefault();
+    }
+}, true);
