0f0bc50fae8eb526c76793e84371d0f1104dd03a
PauloLeal <paulolealdev@gmail.com>
2023-02-11 12:21:29 -0300

    clean app

--- safari-noescape-fullscreen Extension/Resources/background.js
+++
@@ -1,8 +0,0 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-
-    if (request.greeting === "hello")
-        sendResponse({ farewell: "goodbye" });
-});
-
-console.log("[safari-noesc] loaded");
---
+++ safari-noescape-fullscreen Extension/Resources/script.js
@@ -0,0 +1,1 @@
+console.log("[safari-noesc] loaded");
